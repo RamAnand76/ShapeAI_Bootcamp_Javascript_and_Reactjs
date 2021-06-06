8f5896905d828e041618bfe2a2323371708a6a52
RAMANAND R <85400361+RamAnand76@users.noreply.github.com>
2021-06-06 20:50:03 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyright @ 2021</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javasript and React.js</h1>
+      <p>
+        THIS WAS AN AMAZING BOOTCAMP TAKENUP BY SHURYA SINHA.WE COVERED
+        EVERYTHING FROM SCRATCH INCLUDING jAVASCRIPT,HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
